11379e59edce3e9af11b6a3a79253098960ed39a
GovindSinghRajput680 <145134257+GovindSinghRajput680@users.noreply.github.com>
2023-10-04 23:54:12 +0530

    Create Theorytical.cpp

---
+++ Theorytical.cpp
@@ -0,0 +1,4 @@
+Q 5 : Infinite
+Q 6 : (i) True
+      (ii) False (like if we have void return type)
+Q 7 The answer of this Q will be depend on the situations But we can't use exactly same function if we want to use same name only then we have to create difference in argument.
